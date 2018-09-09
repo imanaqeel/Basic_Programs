2348350ada0c4cb05e5ff87c39ed4f933b0d5c96
Saarthak Gupta <saarthakgupta08@gmail.com>
2018-09-09 13:10:51 +0530

    Create xencrypt.cpp

---
+++ CPP/xencrypt.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+
+using namespace std;
+
+string xencrypt(string c)
+{
+	int i;	
+	for(i=0;i<100;i++)
+	{
+		c[i]=c[i]^31;
+	}
+	return c;
+}
